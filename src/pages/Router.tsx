df624e50392489e049e3996b4bba497b3a82e519
danielpvpao <danielpvpao@gmail.com>
2024-10-10 15:14:04 -0300

    ...

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -2,14 +2,14 @@ import {Routes, Route } from 'react-router-dom'
 import { Home } from '../components/Home';
 import { DefaultLayout } from '../layouts/DefaultLayout';
 import { Checkout } from './Checkout';
-import { Succes } from './Succes';
+import { Success } from './Succes';
 export function Router(){
     return( 
         <Routes>
             <Route path='/' element={<DefaultLayout />}>
             <Route path="/" element={<Home />} />
             <Route path='/Checkout' element={<Checkout/>}/>
-            <Route path='/Success' element={<Succes/>}/>
+            <Route path='/Success' element={<Success/>}/>
             </Route>
         </Routes>
     );
